b847fb1f00946cddedac5aed4d510d7a76a831ee
ekonto1010 <ekonto1010@example.com>
2026-05-10 00:07:20 +0200

    Aktualizacja danych i kodu aplikacji

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/projekt-baza/', // Tu wpisz dokładnie nazwę swojego repo na GitHubie
+  base: '/oferta-aut/', // Tu wpisz dokładnie nazwę swojego repo na GitHubie
 })
