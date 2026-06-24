771fe5a212d681869447a16333b252fb4bd5d704
ekonto1010 <ekonto1010@example.com>
2026-06-24 10:55:19 +0200

    Updates

--- src/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/oferta-aut/', // Tu wpisz dokładnie nazwę swojego repo na GitHubie
-})
